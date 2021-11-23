7a36fefc2486f6de5a0a1a82efba948637afdd2e
Cole Paris <coleparis2021@gmail.com>
2021-11-22 23:43:16 -0800

    added parsing with jackson. Created Coin model that holds coin abbreviation and full spelled out name, added getAllCoins valueOf methods to DAO

--- src/main/java/com/revature/crypto/daos/CoinbaseDAO.java
+++ src/main/java/com/revature/crypto/daos/CoinbaseDAO.java
@@ -1,8 +1,15 @@
 package com.revature.crypto.daos;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.crypto.models.Coin;
+
 import java.net.*;
 import java.io.*;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /*
     -methods pull information from one of two coinbase apis (links below) and return a string formatted like a json.
@@ -10,6 +17,11 @@ import java.util.ArrayList;
     -code 200 means success
  */
 public class CoinbaseDAO {
+    ObjectMapper mapper;
+
+    public CoinbaseDAO(){
+        mapper = new ObjectMapper();
+    }
 
     private String getData(String urlText) {
         try {
@@ -48,6 +60,84 @@ public class CoinbaseDAO {
         }
     }
 
+    public double valueOf(String coin){
+        try {
+            String json = getBuyPrice_V2(coin);
+            System.out.println(json);
+            JsonNode jsonNode = mapper.readTree(json);
+            double value = Double.parseDouble(jsonNode.get("data").get("amount").textValue());
+            return value;
+        }catch(Exception e){
+            System.out.println("Trouble parsing the data");
+            return -1;
+            //TODO handle and log exception
+        }
+    }
+
+    public Coin[] getAllCoins(){
+        try{
+            List<Coin> pairs = new ArrayList<>();
+            String json = getSupportedCurrencies_V2();
+            String cutJson = json.substring(8, json.length()-1);
+            //JsonNode jsonNode = mapper.read
+            //Coin coin = mapper.readValue(cutJson, Coin.class);
+            //System.out.println(coin.getName());
+            Coin[] coins = mapper.readValue(cutJson, Coin[].class);
+            //pairs = Arrays.asList(mapper.readValue(cutJson, Coin[].class));
+
+            return coins;
+        } catch(Exception e){
+            System.out.println("Trouble parsing the data");
+            return null;
+            //TODO handle and log exception
+        }
+
+    }
+
+    public double getConversion(String coin, double amount){
+        try {
+            String json = getExchangeRates_V2();
+            String cutJson = json.substring(34, json.length()-2);//natively, the json file is nested. This cuts out the fluff
+            JsonNode jsonNode = mapper.readTree(cutJson);
+            double value = Double.parseDouble(jsonNode.get(coin).textValue());
+            System.out.println(coin+": "+value+" per dollar");
+            return (1/value)*amount;
+        }catch(Exception e){
+            System.out.println("Trouble parsing the data");
+            return -1;
+            //TODO handle and log exception
+        }
+    }
+
+    private boolean parse(String data){
+        String cutData = data.substring(35, data.length()-3);//natively, the json file is nested. This cuts out the fluff
+        System.out.println(cutData);
+
+
+        return false;
+    }
+
+    private boolean parseOLD(String data){
+        try {
+            String newData = data.substring(34, data.length()-2);//natively, the json file is nested. This cuts out the fluff
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode jsonNode = mapper.readTree(newData);
+            JsonNode jsonNode1 = mapper.readTree(data);
+            boolean isArray = jsonNode1.get("data").get("rates").isArray();
+            System.out.println("isArray: "+isArray);
+            String json = jsonNode.get("MANA").textValue();//get("data").get("rates").get("AED").textValue();
+            System.out.println(json);
+            //mapper.writeValue(new File("mapperValue.txt"), json);
+            //"[{\"currency\":\"USD\", \"rates\":38}, {\"name\":\"laplap\", \"age\":5}]";
+            List<String> list = mapper.readValue(json, new TypeReference<List<String>>() {});
+            return false;
+        }catch(Exception e){
+            System.out.println("Trouble parsing the data");
+            return false;
+            //TODO handle and log exception
+        }
+    }
+
     /*
     Note: Coinbase has two separate apis that this application uses. They seem to come from the same database and even have
     similar or repeating methods. For instance, both apis have a call that returns a json of all crypto-currencies.
@@ -64,12 +154,11 @@ public class CoinbaseDAO {
         return getData("https://api.coinbase.com/v2/currencies");
     }
 
-    public String[] getExchangeRates_V2(){
+    public String getExchangeRates_V2(){
         String data = getData("https://api.coinbase.com/v2/exchange-rates");
-        System.out.println(data);
-        String[] elements = data.split(",");
-        return elements;
+        return data;
     }
+
 
     public String getBuyPrice_V2(String currency_pair){//ex) BTC-USD
         String url = "https://api.coinbase.com/v2/prices/"+currency_pair+"/buy";
@@ -81,25 +170,12 @@ public class CoinbaseDAO {
       https://docs.cloud.coinbase.com/exchange/reference/
       All methods coming from this API are tagged with _E (exchange, as shown in url)
      */
-    public String getTradingPairs_E(){
-        return getData("https://api.exchange.coinbase.com/products");
-    }
-
-    public String getProductStats_E() {
-        return getData("https://api.exchange.coinbase.com/products/BTC-USD/stats");
-    }
-
-    //for debugging and visualizing data. Will eventually be removed
-    private void writeToFile(String file){
-        try {
-            FileWriter myWriter = new FileWriter("exchange_rates.json");
-            myWriter.write(file);
-            myWriter.close();
-            System.out.println("Successfully wrote to the file.");
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
-    }
+//    public String getTradingPairs_E(){
+//        return getData("https://api.exchange.coinbase.com/products");
+//    }
+//
+//    public String getProductStats_E() {
+//        return getData("https://api.exchange.coinbase.com/products/BTC-USD/stats");
+//    }
 
 }
