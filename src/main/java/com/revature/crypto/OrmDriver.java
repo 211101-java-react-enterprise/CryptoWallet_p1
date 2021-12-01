d3830f9c06fd37be1e5e703c88db097fc731cdcf
Cole Paris <coleparis2021@gmail.com>
2021-12-01 15:45:12 -0800

    findById implementation

--- src/main/java/com/revature/crypto/OrmDriver.java
+++ src/main/java/com/revature/crypto/OrmDriver.java
@@ -1,6 +1,7 @@
 package com.revature.crypto;
 
 import com.revature.CryptoORM_P1.mapper.SQLMapper;
+import com.revature.crypto.daos.UserDAO;
 import com.revature.crypto.models.Coin;
 import com.revature.crypto.models.User;
 
@@ -19,6 +20,7 @@ public class OrmDriver {
         }
         SQLMapper.setProperties(props);
         SQLMapper mapper = SQLMapper.getInstance();
+        UserDAO userDAO = new UserDAO();
         String id = "5256d1e5-3393-4640-8b9b-44f607a2ec84";
         String username= "namebozies";
         String password = "password3";
@@ -32,18 +34,21 @@ public class OrmDriver {
 //
 //        System.out.println(rowsInserted);
 
-        ResultSet rs = mapper.joinSelect(user, Coin.class, "user_uuid", "user_uuid", "user_uuid", "user_uuid");
-        try{
-            while(rs.next()){
-                System.out.println("amount: "+rs.getString("amount"));
-                System.out.println("name: "+rs.getString("username"));
-                System.out.println("pair: "+rs.getString("currency_pair"));
-                System.out.println("\n-------------------------------------------");
-            }
+//        ResultSet rs = mapper.joinSelect(user, Coin.class, "user_uuid", "user_uuid", "user_uuid", "user_uuid");
+//        try{
+//            while(rs.next()){
+//                System.out.println("amount: "+rs.getString("amount"));
+//                System.out.println("name: "+rs.getString("username"));
+//                System.out.println("pair: "+rs.getString("currency_pair"));
+//                System.out.println("\n-------------------------------------------");
+//            }
+//
+//        } catch(Exception e){
+//            System.out.println();
+//        }
 
-        } catch(Exception e){
-            System.out.println();
-        }
+        User user1 = userDAO.findById(user.getUserId());
+        System.out.println("user1 username: "+user1.getUsername());
 
         //mapper.update(user, "user_uuid");
         //mapper.delete(user, "user_uuid");
--- src/main/java/com/revature/crypto/daos/UserDAO.java
+++ src/main/java/com/revature/crypto/daos/UserDAO.java
@@ -3,6 +3,7 @@ package com.revature.crypto.daos;
 
 
 import com.revature.CryptoORM_P1.mapper.SQLMapper;
+import com.revature.crypto.exceptions.InvalidRequestException;
 import com.revature.crypto.models.User;
 
 import java.io.IOException;
@@ -23,6 +24,8 @@ public class UserDAO implements CrudDAO<User> {
         try {
             ClassLoader loader = Thread.currentThread().getContextClassLoader();
             props.load(loader.getResourceAsStream("db.properties"));
+            SQLMapper.setProperties(props);
+            mapper = SQLMapper.getInstance();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -41,10 +44,10 @@ public class UserDAO implements CrudDAO<User> {
     }
 
     @Override
-    public User save(User newUser) {
-
-        return null;
-
+    public boolean save(User newUser) {
+        int status = mapper.insert(newUser);
+        if(status!=-1) return true;
+        else return false;
     }
 
     @Override
@@ -54,12 +57,29 @@ public class UserDAO implements CrudDAO<User> {
 
     @Override
     public User findById(String id) {
+        ResultSet rs = mapper.select(new User(id, "", "", "", "", -1), "user_uuid");
+        User newUser;
+        try{
+            if(rs.next()){
+                return new User(rs.getString("user_uuid"),
+                        rs.getString("username"),
+                        rs.getString("password"),
+                        rs.getString("first_name"),
+                        rs.getString("password"),
+                        rs.getDouble("dollars_invested"));
+            }
+        }catch(Exception e) {
+            throw new InvalidRequestException("No matching Id found in database");
+        }
         return null;
+
     }
 
     @Override
     public boolean update(User updatedObj) {
-        return false;
+        int status = mapper.update(updatedObj, "user_uuid");
+        if(status!=-1) return true;
+        else return false;
     }
 
     @Override
